e021d1229311719cd15aea09f4f7d22cbd246318
Fyodor Loenko <fedor.loenko@scorpion.co>
2020-07-29 13:03:02 +0500

    feat() add swagger features

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  /**
+   * add Swagger documentation for endpoints
+   */
+  const options = new DocumentBuilder()
+    .setTitle('Distilled Paths API')
+    .setDescription('REST API for Distilled Paths application')
+    .setVersion('1.0')
+    .addTag('paths')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
+
   app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
--- src/paths/paths.controller.ts
+++ src/paths/paths.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { PathsService } from './paths.service';
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('paths')
 @Controller('paths')
 export class PathsController {
   constructor(private pathsService: PathsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get available learning paths' })
+  @ApiResponse({
+    status: 200,
+    description: 'The list of available learning paths with corresponding courses'
+  })
   getPaths() {
     return this.pathsService.getPaths();
   }
