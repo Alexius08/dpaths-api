6927eeedefc62117a321250523a8d67997f00b63
Fyodor Loenko <fedor.loenko@scorpion.co>
2020-09-16 14:10:07 +0500

    feat() scaffold user service methods

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,25 +1,45 @@
-import { Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Logger, Post } from "@nestjs/common";
+import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 
 import { UserService } from './user.service';
-import { ApiTags } from "@nestjs/swagger";
+import { UserDto } from "./user.dto";
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
+  private logger = new Logger('UserController');
+
   constructor(private userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get list of users' })
+  @ApiResponse({
+    status: 200,
+    description: 'The list of DP app users',
+  })
   getUsers() {
     return this.userService.getUsers();
   }
 
   @Post('login')
-  logIn() {
-    return this.userService.logIn();
+  @ApiOperation({ summary: 'Log existing user in' })
+  @ApiResponse({
+    status: 200,
+    description: 'Is the user logged in successfully',
+  })
+  logIn(@Body() userData: UserDto) {
+    this.logger.log(JSON.stringify(userData));
+    return this.userService.logIn(userData);
   }
 
   @Post('signup')
-  signUp() {
-    return this.userService.signUp();
+  @ApiOperation({ summary: 'Sign new user up' })
+  @ApiResponse({
+    status: 200,
+    description: 'Is the user signed up successfully',
+  })
+  signUp(@Body() userData: UserDto) {
+    this.logger.log(JSON.stringify(userData));
+    return this.userService.signUp(userData);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { UserDto } from './user.dto';
+import { UserEntity } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(UserEntity) private userRepository: Repository<UserEntity>) {}
+
+  async getUsers() {
+    const users = await this.userRepository.find();
+    return users;
+  }
+
+  logIn(userData: UserDto) {}
+
+  signUp(userData: UserDto) {}
+}
