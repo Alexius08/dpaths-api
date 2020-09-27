a77e9bb2513677b096e9f417815a6cddacfbd4d3
Fyodor Loenko <fedor.loenko@scorpion.co>
2020-09-27 21:38:58 +0500

    feat() make courses work

--- src/course/course.dto.ts
+++ src/course/course.dto.ts
@@ -1,3 +1,14 @@
-export class CourseDto {
+import { IsOptional, IsString } from 'class-validator';
 
+export class CourseDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  icon?: string;
+
+  @IsString()
+  @IsOptional()
+  objective?: string;
 }
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,10 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { CourseEntity } from './course.entity';
+import { CourseDto } from './course.dto';
+import { ID } from '../shared/models/id.model';
+import { DeleteResponse } from '../shared/models/delete-response.model';
 
 @Injectable()
 export class CourseService {
   constructor(@InjectRepository(CourseEntity) private courseRepository: Repository<CourseEntity>) {}
+
+  async getCourses(): Promise<CourseEntity[]> {
+    return this.courseRepository.find();
+  }
+
+  async createCourse(data: CourseDto): Promise<CourseEntity> {
+    const course = this.courseRepository.create(data);
+    await this.courseRepository.save(course);
+    return course;
+  }
+
+  async getCourse(courseId: ID): Promise<CourseEntity> {
+    const course = await this.courseRepository.findOne({ where: { courseId } });
+    if (!course) {
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    }
+    return course;
+  }
+
+  async updateCourse(courseId: ID, data: CourseDto): Promise<CourseEntity> {
+    const course = await this.courseRepository.findOne({ where: { courseId } });
+    if (!course) {
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    }
+    await this.courseRepository.update({ courseId }, data);
+    return this.courseRepository.findOne({ where: { courseId } });
+  }
+
+  async deleteCourse(courseId: ID): Promise<DeleteResponse> {
+    const course = await this.courseRepository.findOne({ where: { courseId } });
+    if (!course) {
+      throw new HttpException('Course not found', HttpStatus.NOT_FOUND);
+    }
+    await this.courseRepository.delete({ courseId });
+    return { deleted: true, id: courseId };
+  }
 }
